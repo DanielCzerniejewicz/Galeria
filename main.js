7bb1b0bf6a9862df55e911b967a736515217b03d
Daniel Czerniejewicz <programowaniedaniel@gmail.com>
2024-10-16 18:02:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const tab = ["1","2","3","4","5"];
+let index = 0;
+
+function rysowanie() {
+    let zdjecie = document.getElementById("zdjecia");
+    zdjecie.innerHTML = `<img src="${tab[index]}.png" alt="Zdjęcie ${index + 1}">`;
+}
+
+function Lewy() {
+    if (index > 0) {
+        index -= 1;
+    } else {
+        index = tab.length - 1;
+    }
+    rysowanie();
+}
+
+function Prawy() {
+    if (index < tab.length - 1) {
+        index += 1;
+    } else {
+        index = 0;
+    }
+    rysowanie();
+}
